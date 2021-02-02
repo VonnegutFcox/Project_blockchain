30762f2a4636c3dfe258722ab4c2514746796afe
VonnegutFcox <levifcox@gmail.com>
2021-02-02 12:49:38 +0300

    trying to emigrate db

--- blockchain/settings.py
+++ blockchain/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-DATABASES['default']['HOST'] = '127.0.0.1'
+# DATABASES['default']['HOST'] = '127.0.0.1'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
